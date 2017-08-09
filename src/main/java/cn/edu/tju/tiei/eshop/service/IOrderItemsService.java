5b2fd892a3d91f21458c8b1c877676bba032488d
zhusihan <zhuchenzi@tju.edu.cn>
2017-08-09 18:54:52 +0800

    OrderItems completed

---
+++ src/main/java/cn/edu/tju/tiei/eshop/service/IOrderItemsService.java
@@ -0,0 +1,11 @@
+package cn.edu.tju.tiei.eshop.service;
+
+import cn.edu.tju.tiei.eshop.model.OrderItems;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface IOrderItemsService {
+    List<OrderItems> getItemsOfOrder(@Param("orderId") long orderid);
+}
+
